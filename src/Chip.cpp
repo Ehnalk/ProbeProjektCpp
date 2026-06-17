b7214863be52228dea1617595a132ab7b7674af3
Jannik Oeschger <oeschgerjannik@gmail.com>
2026-06-17 13:18:15 +0200

    addded chip.cpp/hpp

---
+++ src/Chip.cpp
@@ -0,0 +1,17 @@
+#include "Chip.hpp"
+
+Chip::Chip(std::string color) {
+    setColor(color);
+}
+
+bool Chip::isEmpty() {
+    return color == "";
+}
+
+void Chip::setColor(std::string color) {
+    if (color == "x" || color == "o" || color == "") {
+        this->color = color;
+    } else {
+        this->color = "";
+    }
+}
